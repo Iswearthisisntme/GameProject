44160eeb20a657fc98e3d1a74a931aacbea78781
Khushi Khan <76977629+khushikhan0@users.noreply.github.com>
2024-02-18 20:04:49 -0500

    monster moves toward player when player isn't looking
    monster looks at the player and moves toward the player when the player isn't looking

---
+++ Assets/Scripts/MonsterBehavior.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterBehavior : MonoBehaviour
+{
+    public Transform player;
+    public float moveSpeed = 0.5f;
+
+    void Start()
+    {
+        if(player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        ReticleEffect();
+    }
+
+    void ReticleEffect() 
+    {
+        RaycastHit hit;
+        float step = moveSpeed * Time.deltaTime;
+
+        if(Physics.Raycast(player.position, player.forward, out hit, Mathf.Infinity))
+        {
+            
+        }
+        else 
+        {
+            transform.LookAt(player);
+            transform.position = Vector3.MoveTowards(transform.position, player.position, step);
+        }
+    }
+}
