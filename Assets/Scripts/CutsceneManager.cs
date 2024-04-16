699d5e334888290fe909345ad326e9148267d745
Iswearthisisntme <jhkimuniversity@gmail.com>
2024-04-16 02:17:39 -0400

    Finished cutscene scene
    Finished cutscene scene

---
+++ Assets/Scripts/CutsceneManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CutsceneManager : MonoBehaviour
+{
+
+    void Update()
+    {
+          if (Input.GetKeyDown(KeyCode.Space))
+        {
+            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(currentSceneIndex + 1);
+        }
+    }
+}
