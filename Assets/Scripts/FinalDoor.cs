74e0a1b6413bca680fe6841c85599609482045f8
Iswearthisisntme <jhkimuniversity@gmail.com>
2024-04-21 13:32:35 -0400

    fixed level loading  bugs
    fixed level loading  bugs

--- Assets/Scripts/FinalDoor.cs
+++ Assets/Scripts/FinalDoor.cs
@@ -6,46 +6,48 @@ public class FinalDoor : MonoBehaviour
 {
 
     private GameObject player;
-    private GameObject door;
+    // private GameObject door;
     private LevelManager levelManager;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-        door = GameObject.FindGameObjectWithTag("FinalDoor");
+        // door = GameObject.FindGameObjectWithTag("FinalDoor");
         levelManager = FindObjectOfType<LevelManager>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetButtonDown("Fire1"))
-        {
-            float distance = Vector3.Distance(door.transform.position, player.transform.position);
-            //print(distance);
-            if (distance < 2.6f) //can adjust range later
-            {
-                levelManager.LevelWon();
-                //AudioSource.PlayClipAtPoint(pluginSFX, Camera.main.transform.position);
-            }
-        }
-    }
+    // void Update()
+    // {
+    //     if (Input.GetButtonDown("Fire1"))
+    //     {
+    //         float distance = Vector3.Distance(door.transform.position, player.transform.position);
+    //         //print(distance);
+    //         if (distance < 2.6f) //can adjust range later
+    //         {
+    //             levelManager.LevelWon();
+    //             //AudioSource.PlayClipAtPoint(pluginSFX, Camera.main.transform.position);
+    //         }
+    //     }
+    // }
 
     private void OnTriggerEnter(Collider other)
     {
         // Checks if player was the colliding object with the pickup
         if (other.CompareTag("Player"))
         {
-            transform.parent = other.transform;
+            levelManager.LevelWon();
 
-            //AudioSource.PlayClipAtPoint(pickupSFX, Camera.main.transform.position);
+            // transform.parent = other.transform;
 
-            gameObject.GetComponent<Animator>().SetTrigger("EndAnimation");
+            // //AudioSource.PlayClipAtPoint(pickupSFX, Camera.main.transform.position);
 
-            //Destroy the pickup object
-            //To be used when the inventory system is fully implemented
-            //Destroy(gameObject);
+            // gameObject.GetComponent<Animator>().SetTrigger("EndAnimation");
+
+            // //Destroy the pickup object
+            // //To be used when the inventory system is fully implemented
+            // //Destroy(gameObject);
         }
     }
 }
--- Assets/Scripts/MonsterUnderBedBehavior.cs
+++ Assets/Scripts/MonsterUnderBedBehavior.cs
@@ -5,16 +5,6 @@ using UnityEngine.UI;
 
 public class MonsterUnderBedBehavior : MonoBehaviour
 {
-    public int startingHealth = 100;
-    public AudioClip deadSFX;
-    
-    public int currentHealth;
-    public float attackRate = 2.0f;
-    public AudioClip monsterDamageSFX;
-
-
-    Animator anim;
-    float elapsedTime = 0;
     Vector3 initialPosition;
     Quaternion initialRotation;
 
@@ -24,28 +14,6 @@ public class MonsterUnderBedBehavior : MonoBehaviour
     {
         initialPosition = transform.position;
         initialRotation = transform.rotation;
-        currentHealth = startingHealth;
-        anim = GetComponent<Animator>();
-    }
-
-    public void TakeDamage(int damageAmount) 
-    {
-        if(currentHealth > 0)
-        {
-            currentHealth -= damageAmount;
-        }
-        
-        if(currentHealth <= 0)
-        {
-            //dead!
-            anim.SetInteger("animState", 0);
-        }
-
-    }
-
-    void Update()
-    {
-        elapsedTime += Time.deltaTime;
     }
 
     void OnTriggerEnter(Collider other) 
@@ -58,20 +26,6 @@ public class MonsterUnderBedBehavior : MonoBehaviour
                 transform.position = Vector3.Lerp(transform.position, initialPosition, 20 * Time.deltaTime);
                 transform.rotation = Quaternion.Lerp(transform.rotation, initialRotation, 20 * Time.deltaTime);
             }
-        }
-
-        if (other.gameObject.CompareTag("Player"))
-        {
-            Debug.Log("player");
-            var PlayerHealth = other.gameObject.GetComponent<PlayerHealth>();
-            if(elapsedTime >= attackRate)
-            {
-                Debug.Log("took damage");
-                AudioSource.PlayClipAtPoint(monsterDamageSFX, Camera.main.transform.position);
-                PlayerHealth.TakeDamage();
-                elapsedTime = 0.0f;
-            }
-            
         }
     }
 }
